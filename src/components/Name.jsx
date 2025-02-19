54eb642e906074a47908afb9fa4525d9433a5b12
HeedlessSoap325 <mathisjulian123@gmail.com>
2025-02-19 13:58:43 +0100

    Added Waehrungsumrechner-Component

--- src/components/Name.jsx
+++
@@ -1,7 +0,0 @@
-export default function Name(){
-    return(
-        <div className={"Name-Component-Header"}>
-            <h1>Mein Nachname ist FRITZ</h1>
-        </div>
-    );
-}
---
+++ src/components/Waehrungsumrechner.jsx
@@ -0,0 +1,7 @@
+export default function Waehrungsumrechner(){
+    return(
+        <div >
+            <h1>Währungsumrechner</h1>
+        </div>
+    );
+}
