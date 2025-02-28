ba4b7d1e5b1fab411255e1b692fa183f1b683644
HeedlessSoap325 <mathisjulian123@gmail.com>
2025-02-28 10:37:44 +0100

    Extended Functionality

--- src/components/Waehrungsumrechner.jsx
+++ src/components/Waehrungsumrechner.jsx
@@ -1,12 +1,23 @@
-import CalculateButton from "./CalculateButton.jsx";
+import {useState} from "react";
 
 export default function Waehrungsumrechner(){
+    const [input_value, setInput_value] = useState(0);
+    const [output_value, setOutput_value] = useState(0);
+
+    function onClickCalculate(){
+        setOutput_value(input_value * 1.1);
+        //document.getElementById("output_area").value = out;
+    }
+
     return(
-        <>
-            <div >
-                <h1>Währungsumrechner</h1>
-            </div>
-            <CalculateButton />
-        </>
+        <div>
+            <input type={"number"} onChange={(e) => setInput_value(Number(e.target.value))}/>
+
+            <button onClick={onClickCalculate}>Calculate</button>
+
+            <textarea id={"output_area"} disabled={true} value={
+                "CHF " + input_value + " sind " + output_value + " in EURO"
+            } />
+        </div>
     );
 }
