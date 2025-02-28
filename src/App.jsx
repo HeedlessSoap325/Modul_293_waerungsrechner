07ab96b4ca60559970d8d36d46328f226e71a58c
HeedlessSoap325 <mathisjulian123@gmail.com>
2025-02-28 10:37:28 +0100

    Fixed App.jsx and added structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import './App.css'
 import Waehrungsumrechner from "./components/Waehrungsumrechner.jsx";
+import Waehrungen from "./assets/Waehrungen.jpg";
+import "./App.css";
 
 export default function App() {
   return (
-    <>
-        <Waehrungsumrechner />
-    </>
+      <div>
+          <div className={"App-Title"}>
+              <h1>Währungsumrechner</h1>
+          </div>
+          <div className={"App-Content"}>
+              <img src={Waehrungen} alt={"Bild von Geld"} width={400}/>
+              <Waehrungsumrechner />
+          </div>
+      </div>
   );
 }
